2467afa474354ca7493fb1a3c28950b14a6c71a6
Oliveira04 <augustocoimbra01@hotmail.com>
2025-05-27 22:13:52 -0300

    Adicionando recurso using namespace std. No projeto

--- grapho2.cpp
+++ grapho2.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <fstream>
+using namespace std;
 
 int main() {
     std::ifstream g2_file("../g2.txt");
 
     if(!g2_file.is_open()){
-        std::cerr << "Error: Could not open g2.txt" << std::endl;
+        cerr << "Error: Could not open g2.txt" << endl;
         return 1;
     }
     std::string line;
-    while (std::getline(g2_file, line)) {
-        std::cout << line << std::endl;
+    while (getline(g2_file, line)) {
+        cout << line << endl;
     }
     g2_file.close();
     return 0;
