fca7e7b6465b789ed38accbb2fc2dd2db650a9d9
Oliveira04 <augustocoimbra01@hotmail.com>
2025-05-28 21:43:44 -0300

    Arrumando os arquivos que deram errado

--- codes/open_file.h
+++
@@ -1,7 +0,0 @@
-// open_file.h
-#ifndef OPEN_FILE_H
-#define OPEN_FILE_H
-
-void processLine(); // Sua declaração está correta
-
-#endif
--- grapho2_main.cpp
+++ grapho2_main.cpp
@@ -1,24 +1,18 @@
 #include <iostream>
 #include <fstream>
-#include <string>     
-#include "open_file.h" //  para conectar com as declarações
-
 using namespace std;
 
-
-
-void processLine(){
+int main() {
     ifstream g2_file("../g2.txt");
 
     if(!g2_file.is_open()){
         cerr << "Error: Could not open g2.txt" << endl;
-        return;
+        return 1;
     }
-    std::string line;
+    string line;
     while (getline(g2_file, line)) {
         cout << line << endl;
     }
     g2_file.close();
-    
+    return 0;
 }
-
